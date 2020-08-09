f97bfff19993bb3c91b6ce3a641d7d8db5f2d749
Makinde Akinola <48059516+Akinolae@users.noreply.github.com>
2020-08-09 09:13:37 +0100

    slight modification

--- services/cashTransfer/cashtransfer.js
+++ services/cashTransfer/cashtransfer.js
@@ -70,6 +70,7 @@ module.exports = {
 
                                     //  The sender's message.
                                      const senderMsg = `
+                                     <h1><strong>${sender[0].firstname} ${sender[0].lastname} ${sender[0].surname}</strong></h1>
                                      <h2  style="color: white; background-color: #2C6975; padding: 30px; width: 50%;"><strong>Afrobank debit alert</strong></h2><br>
                                      <p>We wish to inform you that a debit transaction just occured on your account with us</p>
                                       
@@ -82,10 +83,11 @@ module.exports = {
                                      <p>Recipient  : <strong>${data[0].accountNumber} ${data[0].firstname} ${data[0].lastname} ${data[0].surname}</strong></p>
 
 
-                                     Thank you for banking with us. 
+                                     Thank you for banking with <strong> Afrobank </strong>. 
                                      `;
 
                                      const recipientMsg = `
+                                      <h1><strong>Dear ${data[0].firstname} ${data[0].lastname} ${data[0].surname}</strong></h1>
                                       <h2 style="color: white; background-color: #2C6975; padding: 30px; width: 50%;"><strong>Afrobank Credit alert</strong></h2><br>
                                       <p>We wish to inform you that a credit transaction just occured on your account with us</p>
 
@@ -94,9 +96,9 @@ module.exports = {
                                          <p>Amount      : <strong>${transactionAmt}</strong></p>
                                          <p>Time        : <strong>${hours} : ${minutes}</strong></p>
                                          <p>Balance     : <strong>NGN ${recievedTransfer}</strong></p>  
-                                         <p>Sender      : <strong>${sender[0].firstname} ${sender[0].lastname} ${sender[0].surname}</strong></p>
+                                         <p>Sender      : <strong>${sender[0].firstname} ${sender[0].lastname} ${sender[0].surname}</strong></p><br>
 
-                                         Thank you for banking with us. 
+                                         Thank you for banking with <strong> Afrobank </strong>. 
                                      `;
                                      db.query(`Update customers set  accountBalance = ${newBalance} where accountNumber = ${sender[0].accountNumber}`, (err, resp) => {
                                          if (err) throw err;
