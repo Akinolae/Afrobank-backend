b4bcabb751349c9b4a50c74779ef59aee0eb6019
Akinolae <makindeakinola22@gmail.com>
2020-09-08 12:51:22 +0100

    made new changes

---
+++ config/database/dbconnect.js
@@ -0,0 +1,20 @@
+'use strict';
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: 'mysql',
+    pool: {
+        max: 1000,
+        min: 10,
+        idle: 1000
+    }
+  }
+);
+
+module.exports.sequelize = sequelize;
