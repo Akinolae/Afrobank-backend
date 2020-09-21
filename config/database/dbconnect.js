ceec25704974f7d40e2f69f2cd5ec42df446814c
Akinolae <makindeakinola22@gmail.com>
2020-09-21 16:59:16 +0100

    made new changes

--- config/database/dbconnect.js
+++ config/database/dbconnect.js
@@ -8,11 +8,13 @@ const url = require("url");
 const config = require('./config.json')[env];
 const herokuUrl = url.parse('process.env[config.use_env_variable]');
 let sequelize;
-if(config.use_env_variable){
-   sequelize = new Sequelize(herokuUrl,{dialect: 'mysql'});
+if (config.use_env_variable) {
+  sequelize = new Sequelize(herokuUrl, null, null, {
+    dialect: 'mysql'
+  });
   return sequelize;
-}else{
-   sequelize = new Sequelize(config)
+} else {
+  sequelize = new Sequelize(config)
 }
 
 module.exports.sequelize = sequelize;
