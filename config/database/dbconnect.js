8e6e53442dcb30805c5fc0ca33f4b25eb3717ee4
Akinolae <makindeakinola22@gmail.com>
2020-09-21 15:34:01 +0100

    made new changes

--- config/database/dbconnect.js
+++ config/database/dbconnect.js
@@ -3,24 +3,14 @@ require("dotenv").config();
 const {
   Sequelize
 } = require("sequelize");
-const url = require("url");
-const optionsProduction = url.parse('process.env.CLEARDB_DATABASE_URL');
-const optionsDevelopment = process.env.DATABASE
-const sequelizeOptions = process.env.NODE_ENV === 'production' ? process.env.CLEARDB_DATABASE_URL : optionsDevelopment
-
-  const sequelize = new Sequelize(
-    sequelizeOptions,
-    process.env.USER,
-    process.env.PASSWORD, {
-      host: process.env.HOST,
-      dialect: 'mysql',
-      pool: {
-        max: 1000,
-        min: 10,
-        idle: 1000
-      }
-    }
-  );
-
+var env = process.env.NODE_ENV || 'development'
+const config = require('./config.json')[env];
+let sequelize;
+if(config.use_env_variable){
+   sequelize = new Sequelize(process.env[config.use_env_variable]);
+  return sequelize;
+}else{
+   sequelize = new Sequelize(config)
+}
 
 module.exports.sequelize = sequelize;
