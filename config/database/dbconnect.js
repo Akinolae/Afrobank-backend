500f9c61e27cc1207e57bf4a8441443ea957149e
Akinolae <makindeakinola22@gmail.com>
2020-09-21 15:46:09 +0100

    bug fixes

--- config/database/dbconnect.js
+++ config/database/dbconnect.js
@@ -7,7 +7,7 @@ var env = process.env.NODE_ENV || 'development'
 const config = require('./config.json')[env];
 let sequelize;
 if(config.use_env_variable){
-   sequelize = new Sequelize(process.env[config.use_env_variable],'mysql');
+   sequelize = new Sequelize(process.env[config.use_env_variable],{dialect: 'mysql'});
   return sequelize;
 }else{
    sequelize = new Sequelize(config)
