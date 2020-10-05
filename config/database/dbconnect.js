6f4995dddf7e7ea19529d345944cfc42a76452ef
Akinolae <makindeakinola22@gmail.com>
2020-10-05 13:42:34 +0100

    new changes

--- config/database/dbconnect.js
+++ config/database/dbconnect.js
@@ -6,10 +6,12 @@ const {
 const env = process.env.NODE_ENV || 'development'
 const url = require("url");
 const config = require('./config.json')[env];
+// console.log(config);
 const herokuUrl = url.parse('process.env[config.use_env_variable]');
+console.log(herokuUrl);
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(herokuUrl);
+  sequelize = new Sequelize(config);
 } else {
   sequelize = new Sequelize(config)
 }
